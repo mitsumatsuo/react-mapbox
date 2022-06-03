868057134625b3a3ecce1448fa939738e4111b76
Cims Admin <mitsuteru.matsuo@findex.co.jp>
2022-06-03 11:37:52 +0900

    add tooltip feature

--- src/Map.js
+++ src/Map.js
@@ -2,11 +2,14 @@ import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import MapboxLanguage from "@mapbox/mapbox-gl-language"; // eslint-disable-line import/no-extraneous-dependencies
 import "./Map.css";
+import Tooltip from './components/Tooltip';
+import ReactDOM from 'react-dom';
 
 mapboxgl.accessToken = process.env.REACT_APP_ACCESS_TOKEN;
 
 const Map = () => {
   const mapContainerRef = useRef(null);
+  const tooltipRef = useRef(new mapboxgl.Popup({ offset: 15 }));
 
   const [lng, setLng] = useState(135.78);
   const [lat, setLat] = useState(38.57);
@@ -23,6 +26,36 @@ const Map = () => {
 
     // Add navigation control (the +/- zoom buttons)
     map.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+    // change cursor to pointer when user hovers over a clickable feature
+    map.on("mouseenter", (e) => {
+      if (e.features.length) {
+        map.getCanvas().style.cursor = "pointer";
+      }
+    });
+    
+    // reset cursor to default when user is no longer hovering over a clickable feature
+    map.on('mouseleave', () => {
+      map.getCanvas().style.cursor = '';
+    });
+    
+    // add tooltip when users mouse move over a point
+    map.on('mousemove', e => {
+      const features = map.queryRenderedFeatures(e.point);
+      if (features.length) {
+        const feature = features[0];
+
+        // Create tooltip node
+        const tooltipNode = document.createElement('div');
+        ReactDOM.render(<Tooltip feature={feature} />, tooltipNode);
+
+        // Set tooltip on map
+        tooltipRef.current
+          .setLngLat(e.lngLat)
+          .setDOMContent(tooltipNode)
+          .addTo(map);
+      }
+    });
 
     map.on("move", () => {
       setLng(map.getCenter().lng.toFixed(4));
